2efbcfc1eebe686e86b3faa3cf3c9beb8f4560ee
Yagnnoz <jensk37@gmail.com>
2022-07-07 10:01:20 +0200

    moved hotes functionality to hotel service

--- src/app/data/hotel.service.ts
+++ src/app/data/hotel.service.ts
@@ -1,11 +1,36 @@
 import { Injectable } from '@angular/core';
-import { Hotel } from './hotel';
 import { Room, status } from './room';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HotelService {
+  rooms: Room[] = [];
 
-  constructor() { }
+  constructor() {
+    this.initRooms();
+    this.randomizeBooking();
+  }
+
+  private initRooms(): void {
+    let i: number;
+
+    for (i = 0; i < 25; i++) {
+      this.rooms.push({ id: (i + 1), location: "", state: status.free })
+    }
+  }
+
+  private randomizeBooking(): void {
+    let rnd: number = 0;
+    let i: number;
+    for (i = 0; i < this.rooms.length; i++) {
+      rnd = Math.floor(Math.random() * 6);
+      if (rnd % 2 == 0) {
+        this.rooms[i].state = status.free;
+      } else {
+        this.rooms[i].state = status.booked;
+      }
+      console.log(`room nr: ${this.rooms[i].id} created. State: ${this.rooms[i].state}`);
+    }
+  }
 }
