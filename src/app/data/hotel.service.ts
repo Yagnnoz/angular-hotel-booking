0f6bf78aa259f2e9e52fb7e834fcb02480d4a519
Yagnnoz <jensk37@gmail.com>
2022-07-07 09:54:30 +0200

    added hotelservice

---
+++ src/app/data/hotel.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { Hotel } from './hotel';
+import { Room, status } from './room';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HotelService {
+
+  constructor() { }
+}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HotelService } from '../data/hotel.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private hotelService: HotelService) { }
 
   ngOnInit(): void {
   }
+
+  
+
 
 }
